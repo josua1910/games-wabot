0afe265c9456d86c0f69522e99ff9f2730fe6226
josua1910 <73385008+josua1910@users.noreply.github.com>
2021-08-25 15:01:03 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,31 +1,40 @@
 global.DeveloperMode = 'false' //true Or false
-global.linkGC = ['https://chat.whatsapp.com/Lb4Emjih98rBiCZiZoS2eM', 'https://chat.whatsapp.com/FrluVyY5kBRIhY6yKfH4DI']
-global.owner = ['6281390658325', '6282256115584', '6285713964963']// Put your number here
-global.mods = ['13479805233'] // Want some help?
+global.linkGC = ['https://chat.whatsapp.com/FRGMiYP2MFD92nasmcK6l0']
+global.owner = ['6282114499086']// Put your number here
+global.mods = [] // Want some help?
 global.prems = [] // Premium user has unlimited limit
 global.APIs = { // API Prefix
   // name: 'https://website'
   nrtm: 'https://nurutomo.herokuapp.com',
   xteam: 'https://api.xteam.xyz',
+  bx: 'https://bx-hunter.herokuapp.com',
+  neoxr: 'https://neoxr-api.herokuapp.com',
   nzcha: 'http://nzcha-apii.herokuapp.com',
   bg: 'http://bochil.ddns.net',
   fdci: 'https://api.fdci.se',
+  bsbt: 'https://bsbt-api-rest.herokuapp.com',
   dzx: 'https://api.dhamzxploit.my.id',
   zahir: 'https://zahirr-web.herokuapp.com',
   zeks: 'https://api.zeks.xyz',
-  pencarikode: 'https://pencarikode.xyz'
+  pencarikode: 'https://pencarikode.xyz',
+  hardianto: 'https://hardianto-chan.herokuapp.com'
 }
 global.APIKeys = { // APIKey Here
   // 'https://website': 'apikey'
   'https://api.xteam.xyz': 'd90a9e986e18778b',
   'https://zahirr-web.herokuapp.com': 'zahirgans',
+  'https://pencarikode.xyz': 'pais',
+  'https://neoxr-api.herokuapp.com': 'yntkts',
+  'https://bx-hunter.herokuapp.com': 'Ikyy69',
+  'https://bsbt-api-rest.herokuapp.com': 'benniismaelapikey',
   'https://api.zeks.xyz': 'apivinz',
-  'https://pencarikode.xyz': 'pais'
+  'https://pencarikode.xyz': 'pais',
+  'https://hardianto-chan.herokuapp.com': 'hardianto'
 }
 
 // Sticker WM
-global.packname = 'wa.me/6285713964963'
-global.author = 'Metro Bot Is The Best'
+global.packname = 'wa.me/6282114499086'
+global.author = 'Benniganteng'
 
 global.multiplier = 39 // The higher, The harder levelup
 
